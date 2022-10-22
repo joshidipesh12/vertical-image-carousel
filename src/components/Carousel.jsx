7079c163700152ac4e2567db484d2d65f3c84472
Dipesh Joshi <joshidipesh12@gmail.com>
2022-10-22 15:32:07 +0530

    automatic image carosel

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,19 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../styles/carousel.css";
 import images from "../data/images.json";
 
 function Carousel() {
+  const [currIndex, setCurrIndex] = useState(0);
+
+  // const prevImage = () => setCurrIndex(currIndex - 1);
+  const nextImage = () => setCurrIndex((p) => (p + 1) % images.length);
+
+  useEffect(() => {
+    const imageTimeout = setInterval(nextImage, 7000);
+    return () => clearTimeout(imageTimeout);
+  }, []);
+
   return (
     <section className="carousel-container">
-      {images.map((item) => (
-        <div
-          key={item.name}
-          className="image-item"
-          style={{
-            backgroundImage: `url("https://picsum.photos/1024/1024?${item.name}")`,
-          }}
-        ></div>
-      ))}
+      <div
+        className="image-item"
+        style={{
+          backgroundImage: `url("https://source.unsplash.com/1024x1024/?${images[currIndex].name}")`,
+        }}
+      >
+        <div className="image-cover"></div>
+      </div>
     </section>
   );
 }
